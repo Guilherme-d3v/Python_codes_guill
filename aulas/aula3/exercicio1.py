a72c2a2883d16430cd4ca10462e062af162005df
Guilherme-d3v <guii.parada@gmail.com>
2024-10-02 20:34:38 -0300

    att de exercicio

---
+++ aulas/aula3/exercicio1.py
@@ -0,0 +1,5 @@
+#calculadora de IPVA
+print("Olá, vamos calcular seu IPVA")
+Valor_carro =float( input("Digite o valor do seu veiculo "))
+Valor_ipva = Valor_carro * 0.04
+print(f"O valor que você deverá pagar de IPVA é {Valor_ipva}")
