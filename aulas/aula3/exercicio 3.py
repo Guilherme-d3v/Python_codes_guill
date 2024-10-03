c46adcc21020c9c309a87659538ad903f56fc2d5
Guilherme-d3v <guii.parada@gmail.com>
2024-10-02 21:44:28 -0300

    Create exercicio 3.py

---
+++ aulas/aula3/exercicio 3.py
@@ -0,0 +1,7 @@
+#tabela para tamanho de sapato e fucinheira para cães 
+print("Olá, vamos identificar o tamanho do sapato e fucinheira para seu pet")
+coleira =  int (input("Qual tamanho da coleira de seu pet ? "))
+sapato = coleira*2
+fucinheira = sapato*3
+print(f"O tamanho ideal de sapato para eu pet é {sapato}\nO tamanho ideal de fucinheira para seu pet é {fucinheira}")
+
