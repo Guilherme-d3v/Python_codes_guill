70642d5cc3404b1eabe91c9f53116bc88ed14086
Guilherme-d3v <guii.parada@gmail.com>
2024-10-02 21:11:07 -0300

    agora eu sei o que to fazendo

--- aulas/aula3/exercicio1.py
+++ aulas/aula3/exercicio1.py
@@ -2,4 +2,4 @@
 print("Olá, vamos calcular seu IPVA")
 Valor_carro =float( input("Digite o valor do seu veiculo "))
 Valor_ipva = Valor_carro * 0.04
-print(f"O valor que você deverá pagar de IPVA é {Valor_ipva}")
+print(f"O valor que você deverá pagar de IPVA é {round(Valor_ipva,2)}")
