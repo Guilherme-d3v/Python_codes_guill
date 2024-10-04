6510525bd64e3d8ca75b61bce1e942ea41473cbe
Guilherme-d3v <guii.parada@gmail.com>
2024-10-04 20:48:02 -0300

    att exerc3

--- aulas/aula 4/exerc3.py
+++ aulas/aula 4/exerc3.py
@@ -1,15 +1,21 @@
 peso = float (input("Olá, diga-nos seu peso "))
 altura = float( input("agora, diga-nos sua altura "))
 imc = (peso/(altura*altura))
+
 if (imc < 18.5):
     print ("magreza")
+
 elif (imc >= 18.5 and imc <= 24.9):
-    print("normal")
+    print(f"seu imc é {imc:.2f} classificado como normal")
+
 elif (imc >24.9 and imc <= 29.9):
-     print ("sobrepeso")
+     print (f"seu imc é {imc:.2f} classificado como sobrepeso")
+
 elif (imc >29.9 and imc <=39.9):
-    print ("obesidade ")
+    print (f"seu imc é {imc:.2f} classificado como obesidade ")
+
 elif (imc >40):
-    print("obesidade grave")
+    print(f"seu imc é {imc:.2f} classificado como obesidade grave")
+
 else:
     print("que que ta acontecendo aqui gente ????")
