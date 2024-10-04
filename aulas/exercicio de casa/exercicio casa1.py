f4b633710d70d8c0f67c4d6bb4b6d2921afe2cb0
Guilherme-d3v <guii.parada@gmail.com>
2024-10-03 21:35:47 -0300

    exercicio feito em casa

---
+++ aulas/exercicio de casa/exercicio casa1.py
@@ -0,0 +1,8 @@
+#recebendo dados necessarios
+aro= int (input ("Olá usuário, diga o tamanho de Aro que deseja comprar para que possamos lhe orientar quanto nos demais periféricos "))
+#calculando tamanho de periféricos 
+guidao= aro/2
+manet=aro/4
+quadro = aro
+#devolvendo ao usuario as dimensoes calculadas
+print (f"Com base no Aro informado\n O tamanho ideal de guidao é {guidao}\n O tamanho ideal de manet é {manet}\n O tamanho ideal de quadro é {quadro}")
