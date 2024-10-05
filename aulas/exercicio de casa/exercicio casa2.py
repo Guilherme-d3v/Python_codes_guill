7c2f73af174d4b21a6ea75f3c17332a8c3b9e75d
Guilherme-d3v <guii.parada@gmail.com>
2024-10-05 19:53:46 -0300

    exerc casa 2

---
+++ aulas/exercicio de casa/exercicio casa2.py
@@ -0,0 +1,15 @@
+# Recebendo variavel para verificação
+num = float ( input ( "entre com o número " ) )
+
+# Declarando condições 
+if  ( num % 3 == 0 and num % 5 > 0):
+    print("o número é divisivel por 3 ")
+
+elif  (num % 5 == 0 and num % 3 > 0):
+    print ("o número é divisivel por 5 ")
+
+elif  (num % 5 == 0 and num % 3 == 0):
+    print ("o número é divisivel por 3 e 5 ")
+
+else :
+    print ("o número nao é divisivel por 3 nem por 5 ")
