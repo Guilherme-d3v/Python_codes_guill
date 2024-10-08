5b7fbc3e35a22326a8c8c3c041608f03553c0dc8
Guilherme-d3v <guii.parada@gmail.com>
2024-10-07 21:43:19 -0300

    att

---
+++ aulas/aula 5/exercicio 2.py
@@ -0,0 +1,16 @@
+print ("Olá,Seja bem vindo a nossa lanchonete")
+pedido = int(input ("você gostaria de escolher produtos individualmente ou optar pelo combo ? \n 1 - escolher produto \n 2- Combo\n"))
+item1 = "hamburguer"
+item2 = "batata frita"
+item3 = "refrigerante"
+item4 = "combo"
+
+if pedido ==1:
+print(f"Escolha seus itens\n 1- hamburguer R$10,00\n 2- batata frita R$ 10,00\n refrigerante R$10,00)")
+escolha1 = input()
+elif (escolha1) == 1:
+escolha2 =input("Gostaria de adicionar algo ao seu pedido ?\n 1- adicionar itens\n 2- concluir pedido ")
+elif escolha2 = 1:
+print ("escolhe aqui")
+else:
+print("pedido concluido")
